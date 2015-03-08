46623d8bd9712295ba8f90afd97e739891576911
flashback2k14 <feedbackcalcest@gmail.com>
2015-03-08 09:57:55 +0100

    upload tex file

--- src/gui/ActionConstants.java
+++
@@ -1,8 +0,0 @@
-package gui;
-
-public class ActionConstants {
-	public final static String NEW = "New";
-	public final static String OPEN = "Open";
-	public final static String CLOSE = "Close";
-	public final static String NEXTSTEP = "Next Step";
-}
